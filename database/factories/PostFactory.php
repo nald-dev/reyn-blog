6d3b53da19ba2445bd6f058d1c1fa2620b65e824
nald-dev <rynvva@gmail.com>
2022-07-25 18:39:36 +0700

    add post model

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => mt_rand(1, 4),
+            'title' => fake()->sentence(mt_rand(2, 4)),
+            'slug' => fake()->unique()->slug(),
+            'excerpt' => fake()->sentence(10, 16),
+            'body' => fake()->paragraph(mt_rand(4, 10)),
+        ];
+    }
+}
